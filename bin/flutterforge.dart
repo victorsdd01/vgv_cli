d31552e7262e3e2ba74757cdb7976c0f7582809b
victorsdd01 <victorsdd01@users.noreply.github.com>
2026-01-24 12:02:13 -0500

    chore: rename package to vgv_cli

--- bin/flutterforge.dart
+++ bin/flutterforge.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_forge_cli/flutterforge_cli.dart';
+import 'package:vgv_cli/flutterforge_cli.dart';
 
 void main(List<String> arguments) async {
   await FlutterForgeCLI().run(arguments);
--- lib/data/datasources/file_system_datasource.dart
+++ lib/data/datasources/file_system_datasource.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import 'package:flutter_forge_cli/core/templates/template_generator.dart';
+import 'package:vgv_cli/core/templates/template_generator.dart';
 import 'package:path/path.dart' as path;
 import '../../domain/entities/project_config.dart';
 
